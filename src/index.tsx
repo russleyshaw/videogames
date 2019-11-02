c03c434932c4494ea50cb64ca69ea78794295996
Russley Shaw <russleyshaw@gmail.com>
2019-11-02 15:39:16 -0500

    Fix scss

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./app_view";
 
-import "./style/style.scss";
 import appStore from "./models/app_store";
 import settingsStore from "./models/settings_store";
 
